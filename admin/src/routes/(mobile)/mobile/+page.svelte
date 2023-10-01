87ecf1f2b8b45b9a6685d29953ec5888a3b20bd0
Nihan <nihanmubashshir@gmail.com>
2023-10-01 10:19:12 +0600

    feat: admin dashboard tailwind and shadcn setup

---
+++ admin/src/routes/(mobile)/mobile/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="container h-screen w-screen flex justify-center items-center">
+	<h1 class="text-2xl text-center border-[1px] rounded-sm p-5">
+		Mobile is not supported please use large tablet, laptop or a desktop.
+	</h1>
+</div>
---
+++ admin/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { error } from '@sveltejs/kit';
+	let isMobile = false;
+	if (browser) {
+		isMobile = window.innerWidth <= 768;
+		if (isMobile) {
+			console.error('Mobile not supported');
+			goto('/mobile');
+		}
+	}
+</script>
+
+<slot />
